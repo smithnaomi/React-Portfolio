680b885288d32e3098ff8ea410b9e894e08549f9
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-02-07 23:25:43 -0500

    default

--- src/resumeData.js
+++ src/resumeData.js
@@ -73,6 +73,38 @@ let resumeData = {
       skillname: "Node",
     },
   ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
 };
 
 export default resumeData;
